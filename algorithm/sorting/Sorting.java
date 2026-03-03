febb441818c80c97844a2b972ac34b97a6675450
disiasd <pietro.dilena@gmail.com>
2026-03-03 15:27:57 +0100

    Add files via upload

---
+++ algorithm/sorting/Sorting.java
@@ -0,0 +1,70 @@
+package algorithm.sorting;
+
+/**
+* This class contains various sorting algorithms
+*/
+public class Sorting {
+
+	/**
+	* Sorts the specified array into ascending numerical order in &Theta;(n<sup>2</sup>)
+	* <p>
+	* Implements the selectionsort algorithm.
+	* <ul>
+	* <li> Worst/Average/Best-case cost: &Theta;(n<sup>2</sup>)
+	* </ul>
+	* @param A the array to be sorted
+	*/
+	public static void selectionsort(int A[]) {
+	}
+
+	/**
+	* Sorts the specified array into ascending numerical order in O(n<sup>2</sup>)
+	* <p>
+	* Implements the insertionsort algorithm.
+	* <ul>
+	* <li> Worst/Average-case cost: &Theta;(n<sup>2</sup>)
+	* <li> Best-case cost: &Theta;(n)
+	* </ul>
+	* @param A the array to be sorted
+	*/
+	public static void insertionsort(int A[]) {
+	}
+
+	/**
+	* Sorts the specified array into ascending numerical order in &Theta;(nlogn)
+	* <p>
+	* Implements the mergesort algorithm.
+	* <ul>
+	* <li> Worst/Average/Best-case cost: &Theta;(nlogn)
+	* </ul>
+	* @param A the array to be sorted
+	*/
+	public static void mergesort(int A[]) {
+	}
+
+	/**
+	* Sorts the specified array into ascending numerical order in O(n<sup>2</sup>) and O(nlogn) on the average
+	* <p>
+	* Implements the quicksort algorithm.
+	* <ul>
+	* <li> Worst-case cost:  &Theta;(n<sup>2</sup>)
+	* <li> Average/Best-case cost: &Theta;(nlogn)
+	* </ul>
+	* @param A the array to be sorted
+	*/
+	public static void quicksort(int A[]) {
+	}
+
+	/**
+	* Sorts the specified array into ascending numerical order in &Theta;(n+k)
+	* <p>
+	* Implements the countingsort algrithm.
+	* <ul>
+	* <li> Worst/Average/Best-case cost: &Theta;(n+k), where k = max(<code>A</code>)-min(<code>A</code>)+1
+	* </ul>
+	* @param A the array to be sorted
+	*/
+	public static void countingsort(int A[]) {
+	}
+	
+}
