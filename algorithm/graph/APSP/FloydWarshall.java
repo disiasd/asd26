7ddf69106bc1c46ceb6e62a9fce8377856c28bde
gianluigizavattaro <44429317+gianluigizavattaro@users.noreply.github.com>
2026-05-07 11:00:09 +0200

    Floyd Warshall updated

--- algorithm/graph/APSP/FloydWarshall.java
+++ algorithm/graph/APSP/FloydWarshall.java
@@ -29,58 +29,9 @@ public class FloydWarshall<D> implements APSP<D> {
 	 */	
 	public  Map<Vertex<D>, Map<Vertex<D>,Edge<D>>> AllPairsShortestPaths(Graph<D> graph) {
 
-		Map<Vertex<D>, Map<Vertex<D>,Edge<D>>> next = 
-				new HashMap<Vertex<D>, Map<Vertex<D>,Edge<D>>>();
-
-		ArrayList<Vertex<D>> vert = graph.vertexes();
-		ArrayList<Edge<D>> edge = graph.edges();
-
-		Map<Vertex<D>, Integer> map = new HashMap<Vertex<D>, Integer>();		
-
-		for (int i = 0; i < vert.size(); i++) {
-			map.put(vert.get(i), i);
-			next.put(vert.get(i), new HashMap<Vertex<D>,Edge<D>>());
-		}
 		
-		double[][] dist = new double[vert.size()][vert.size()];
 		
-		for (int i=0; i<vert.size(); i++) {
-			for (int j=0; j<vert.size(); j++) {
-				if (i==j) 
-					dist[i][j] = 0.0;
-				else 
-					dist[i][j] = Double.POSITIVE_INFINITY;
-					next.get(vert.get(i)).put(vert.get(j),null);
-			}
-		}
-		
-		for (int l=0; l<edge.size(); l++) {
-			Edge<D> e = edge.get(l);
-			int s = map.get(e.getSource());
-			int d = map.get(e.getDest());
-			dist[s][d] = e.getWeight();
-			next.get(vert.get(s)).put(vert.get(d),e);
-		}
-		
-		for (int k=0; k<vert.size(); k++) {
-			for (int x=0; x<vert.size(); x++) {
-				for (int y=0; y<vert.size(); y++) {
-					if (dist[x][k]+dist[k][y] < dist[x][y]) {
-						dist[x][y] = dist[x][k]+dist[k][y];
-						next.get(vert.get(x)).
-							put(vert.get(y), next.get(vert.get(x)).get(vert.get(k)));
-					}
-				}		
-			}
-		}
-		// negative cycles check
-		for (int x=0; x<vert.size(); x++) {
-			if  (dist[x][x] < 0) {
-				return null;
-			}
-		}
-		
-		return next;	
+		return null;	
 	}
 
 }
